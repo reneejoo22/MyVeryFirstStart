0c11a9b5f3cf399084c97111c6674ed1af1f7e64
Renee Joo <renee.joo03@gmail.com>
2023-07-16 18:49:56 +0900

    Revert "Update ConsoleApplication1.cpp"
    This reverts commit 55c3cb3fb463b2c88fa0e95996cd7a8c8be774bf.

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -8,8 +8,6 @@ int main()
     std::cout << "Hello World!\n";
     std::cout << "Can You see me?";
     std::cout << "\nYes I see u";
-    
-    std::cout << "\nNow I'm seeing u in GitHub";
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
